a1c8332d2a6562881833d00b5e45636aa9494638
GreedInc <fabien.escourbiac@gmail.com>
2025-09-13 12:46:50 +0200

    update for td that will occur 10-2025 (added new exercises)

--- lib/src/test/java/com/rodez/demo/FizzBuzzTest.java
+++ lib/src/test/java/com/rodez/demo/FizzBuzzTest.java
@@ -1,16 +1,20 @@
 package com.rodez.demo;
 
+/*import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;*/
+
 /**
- * The {@code FizzBuzzTest} class contains JUnit tests for the {@link FizzBuzz}
- * class. It verifies the correctness of the FizzBuzz algorithm by testing
- * various input scenarios.
+ * The {@code FizzBuzzTest} class contains JUnit tests for the {@link FizzBuzz} class. It verifies
+ * the correctness of the FizzBuzz algorithm by testing various input scenarios.
  *
- * <p>This test class follows the standard JUnit testing conventions to ensure
- * the reliability and effectiveness of the FizzBuzz implementation.
+ * <p>This test class follows the standard JUnit testing conventions to ensure the reliability and
+ * effectiveness of the FizzBuzz implementation.
  *
  * @author <your name>
  * @version 1.0
  */
 public class FizzBuzzTest {
 
+  /*@Test
+  public void playWithZero() {}*/
 }
